c33d8373f4515e55245497f122dfee741fb3ad3b
SEOUNGROK SHIN <tnsfhr03@gmail.com>
2024-08-12 17:54:01 +0900

    240812 Stack Algorithm

--- src/chap03/IdTester.java
+++ src/chap03/IdTester.java
@@ -5,9 +5,7 @@ package chap03;
         private int id;
 
         // 생성자 (생성자)
-        public void Id() {
-            id = ++counter;
-        }
+        public Id() { id = ++counter; }
 
         // counter를 반환 메서드 (클래스 메서드)
         public static int getCounter() {
@@ -18,11 +16,15 @@ package chap03;
     }
 
     public class IdTester {
+
         public static void main(String[] args) {
             Id a = new Id(); // id 1
             Id b = new Id(); // id 2
 
-            System.out.println("부여한 아읻의 개수: " + Id.getCounter());
+            System.out.println("a의 아이디: " + a.getId());
+            System.out.println("b의 아이디: " + b.getId());
+
+            System.out.println("부여한 아이디의 개수: " + Id.getCounter());
         }
 }
 
