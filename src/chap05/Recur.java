f6ed1897db045c5531444d0c88e666ba151cac90
SEOUNGROK SHIN <tnsfhr03@gmail.com>
2024-08-23 15:46:41 +0900

    240823 Algorithm Memorization recur

--- src/chap05/Recur.java
+++ src/chap05/Recur.java
@@ -3,21 +3,35 @@ package chap05;
 import java.util.Scanner;
 
 public class Recur {
+
+    static int count;
+
     // 재귀 함수
     static void recur(int n) {
         if (n > 0) {
             recur(n - 1);
             System.out.println(n);
             recur(n - 2);
+            count++;
         }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("정수를 입력하세요. ");
-        int x = sc.nextInt();
+        int n = sc.nextInt();
 
-        recur(x);
+        recur2(n);
+        System.out.print("count: " + count);
+//        recur2(n);
+    }
+
+    static void recur2(int n) {
+        if (n > 0) {
+            recur2(n - 2);
+            System.out.println(n);
+            recur2(n -1);
+            count++;
+        }
     }
 }
