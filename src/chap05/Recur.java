1386d059713b4cf0fdb610f222df4b4e51a2784a
SEOUNGROK SHIN <tnsfhr03@gmail.com>
2024-08-26 17:46:56 +0900

    240826 Algorithm hanoi Recur.java

--- src/chap05/Recur.java
+++ src/chap05/Recur.java
@@ -10,8 +10,8 @@ public class Recur {
     static void recur(int n) {
         if (n > 0) {
             recur(n - 1);
-            System.out.println(n);
             recur(n - 2);
+            System.out.println(n);
             count++;
         }
     }
@@ -21,17 +21,14 @@ public class Recur {
         System.out.print("정수를 입력하세요. ");
         int n = sc.nextInt();
 
-        recur2(n);
-        System.out.print("count: " + count);
-//        recur2(n);
+        recur3(n);
     }
-
-    static void recur2(int n) {
-        if (n > 0) {
-            recur2(n - 2);
+    // 재귀함수 X
+    static void recur3(int n) {
+        while(n > 0) {
+            recur3(n - 1);
             System.out.println(n);
-            recur2(n -1);
-            count++;
+            n = n - 2;
         }
-    }
-}
+      }
+  }
