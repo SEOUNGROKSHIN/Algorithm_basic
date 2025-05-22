ccb1fcb3a465fbba7f13203bc8f0388b4390e803
신승록 <tnsfhr03@gmail.com>
2025-05-22 11:23:07 +0900

    250522 프로그래머스 초기 설정

---
+++ src/Programmers.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        System.out.println("테스트");
+        return "";
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        sol.solution("abcdef", 1, 4);
+    }
+}
