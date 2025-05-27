461e24a9910823e8644d1be58e6b85d9c2c50565
신승록 <tnsfhr03@gmail.com>
2025-05-27 17:33:49 +0900

    250527 Programmers 파일 수정

---
+++ src/Application.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(int q, int r, String code) {
+        String answer  = "";
+
+        return answer;
+    }
+}
+
+public class Application {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String result = sol.solution(3, 1, "qjnwezgrpirldywt");
+        System.out.println(result);  // 출력 확인
+    }
+}
